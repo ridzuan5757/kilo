0d9dea65777952eaee9e6dbdf5bdc588744b825b
Muhamad Ridzuan <ridzuan5757@Muhamads-MacBook-Pro.local>
2024-02-17 08:16:34 +0800

    main

--- kilo.c
+++ kilo.c
@@ -1,1 +1,10 @@
-int main() { return 0; }
+int main() {
+
+  /**
+   * Running `echo $?` after the program finishes running will return the value
+   * of this return statement. This command display the exit status of the
+   * previous command. This can be useful to finds any errors in the code at
+   * each compilation.
+   */
+  return 0;
+}
