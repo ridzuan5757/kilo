0445cce6d46176129f531e18f7b869b60216b922
Muhamad Ridzuan <ridzuan5757@Muhamads-MacBook-Pro.local>
2024-02-17 08:21:07 +0800

    read input

--- kilo.c
+++ kilo.c
@@ -1,4 +1,18 @@
+#include <unistd.h>
+
 int main() {
+
+  /**
+   * `read()` and `STDIN_FILENO` come from `<unistd.h>`. We are asking `read()`
+   * to read 1 bute from the standard input into the variable `c`, and to keep
+   * doing it until there are no more bytes to be read.
+   *
+   * `read()` returns the number of bytes that it read, and will return `0` when
+   * it reaches the end of the file.
+   */
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1)
+    ;
 
   /**
    * Running `echo $?` after the program finishes running will return the value
