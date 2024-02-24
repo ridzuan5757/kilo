fdc8fc82de7b130307fac2368a66b8d71941fc9b
ridzuan5757 <ridzuan5757@protonmail.com>
2024-02-24 10:26:00 +0800

    echo control implementation

--- kilo.c
+++ kilo.c
@@ -1,7 +1,18 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~ECHO;
+  tcsetattr(STDIN_FILENO, TCAFLUSH, &raw);
+}
+
 int main() {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
+  return 0;
 }
