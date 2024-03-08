9315430db96a4bf996ec2e9e8f00a2deff0ca814
ridzuan5757 <ridzuan5757@gmail.com>
2024-03-08 23:23:26 +0800

    window size

--- kilo.c
+++ kilo.c
@@ -66,12 +66,61 @@ innt getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[998B", 12) != 12) {
+      editorReadKey();
+      return -1;
+    }
+
     return -1;
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
     return 0;
   }
+}
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  /* printf("\r\n"); */
+  /* char c; */
+  /* while (read(STDIN_FILENO, &c, 1) == 1) { */
+  /*   if (iscntrl(c)) { */
+  /*     printf("%d\r\n", c); */
+  /*   } else { */
+  /*     printf("%d ('%c')\r\n", c, c); */
+  /*   } */
+  /* } */
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[1], 1) != 1) {
+      break;
+    }
+
+    if (buf[i] == 'R') {
+      break;
+    }
+
+    i++;
+  }
+  buf[i] = '\0';
+
+  // printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+  if (buf[0] != '\x1b' || buf[1] != '[') {
+    return -1;
+  }
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+    return -1;
+  }
+
+  // editorReadKey();
+  return -1;
 }
 
 void editorProcessKeypress() {
