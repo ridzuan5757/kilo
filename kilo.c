f1a474e7708322d192ffadcd15443e4214f8ee28
ridzuan5757 <ridzuan5757@gmail.com>
2024-03-15 00:05:26 +0800

    windows size

--- kilo.c
+++ kilo.c
@@ -2,12 +2,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) (k & 0x1f)
 
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -61,10 +64,23 @@ char editorReadKey(void) {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+  }
+
+  return 0;
+}
+
 void editorDrawRows(void) {
   int y;
 
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -92,8 +108,15 @@ void editorProcessKeypress(void) {
   }
 }
 
+void initEditor(void) {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+    die("getWindowSize");
+  }
+}
+
 int main(void) {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
