672479dc9b1a2d4dcab24d598a614a2e560a4e56
ridzuan5757 <ridzuan5757@gmail.com>
2024-03-15 23:14:23 +0800

    window size v2

--- kilo.c
+++ kilo.c
@@ -64,11 +64,53 @@ char editorReadKey(void) {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  // VT100 n - cursor position report
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+      break;
+    }
+
+    if (buf[i] == 'R') {
+      break;
+    }
+
+    i++;
+  }
+
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') {
+    return -1;
+  }
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+    return -1;
+  }
+
+  editorReadKey();
+  return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    // VT100 B - cursor forward
+    // VT100 C - cursor down
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+      return -1;
+    }
+
+    return getCursorPosition(rows, cols);
+
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
