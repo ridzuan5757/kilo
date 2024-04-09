8e5ff9effb66c8d0c75b56d730b6d44ca3c42cfe
ridzuan5757 <ridzuan5757@gmail.com>
2024-04-09 08:05:01 +0800

    implementation of tab rendering

--- kilo.c
+++ kilo.c
@@ -14,6 +14,8 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) (k & 0x1f)
 
+#define KILO_TAB_STOP 8
+
 enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
@@ -28,7 +30,9 @@ enum editorKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
@@ -211,6 +215,36 @@ int getWindowSize(int *rows, int *cols) {
   return 0;
 }
 
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      tabs++;
+    }
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + (tabs * (KILO_TAB_STOP - 1)) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+
+      while (idx % KILO_TAB_STOP != 0) {
+        row->render[idx] = ' ';
+        idx++;
+      }
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -220,6 +254,11 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -315,7 +354,7 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
 
       if (len < 0) {
         len = 0;
@@ -324,7 +363,7 @@ void editorDrawRows(struct abuf *ab) {
       if (len > E.screencols) {
         len = E.screencols;
       }
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     // VT100 K - erase in line
